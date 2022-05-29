27d1d188615d066e7032e2bd32cc48cff653c208
Shinono <85401098+K93MSY@users.noreply.github.com>
2022-05-29 12:47:02 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -7,10 +7,19 @@ var listener = app.listen(3000,function(){
 
     app.use(express.static('sounds'));
 
+    //var sn=require('./sounds-info.json');
+    //var readinfo=JSON.parse(sn);
+    //var ana = Math.random(readinfo.nm);
+    var ana;
+    const data = require('./sounds-info.json');
+
+    for (const key in data) {
+        ana=data[key];
+    }          
+    ana = Math.ceil(ana);
+    app.use('/24', express.static(__dirname + '/sounds/an/' + ana + '.mp3'));
+    app.use('/24/', express.static(__dirname + '/sounds/an/' + ana + '.mp3'));
+
     app.use('/', express.static(__dirname + '/sounds'));
-
-
-    var an =0;
-    app.use('/24', express.static(__dirname + '/sounds/an/' + an + '.mp3'));
 
 });
